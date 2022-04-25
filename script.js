c83febe644ee170c4597872202a78a27b1100bd0
Diana V <dmvasquezr@hotmail.com>
2022-04-25 18:55:59 -0500

    Changes

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-let mySong = document.getElementById("mySong");
-let icon = document.getElementById("icon");
+// esta funcion debe estar en el click del icono
+function handlePlay($event) {
+  // tener el mismo id para la imagen y el audio
+  const id = $event.target.id;
+ 
+  // obtener el elemento img con el id
+  const img = document.querySelector(`img[id='${id}']`);
 
-icon.onclick = function() {
-  if (mySong.paused) {
-    mySong.play();
-    icon.src = "./assets/pause.png";
+  // obtener el elemento audio con el id
+  const audio = document.querySelector(`audio[id='${id}']`);
 
+  if (audio.paused) {
+    audio.play();
+    img.src = "./assets/pause.png";
   } else {
-    mySong.pause();
-    icon.src = "./assets/play.png";
-   
+    audio.pause();
+    img.src = "./assets/play.png";
   }
 }
